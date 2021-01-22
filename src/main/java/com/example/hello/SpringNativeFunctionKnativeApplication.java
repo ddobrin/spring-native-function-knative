3c8794b294dc81ddc39633ed3e51cefe7320aae6
Dan Dobrin <dan.dobrin@gmail.com>
2021-01-22 16:05:41 -0500

    First commit

---
+++ src/main/java/com/example/hello/SpringNativeFunctionKnativeApplication.java
@@ -0,0 +1,26 @@
+package com.example.hello;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Function;
+
+@SpringBootApplication
+public class SpringNativeFunctionKnativeApplication {
+
+    @Value("${TARGET:from-function}")
+    String target;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNativeFunctionKnativeApplication.class, args);
+    }
+
+    @Bean
+    public Function<String, String> hello() {
+        return (in) -> {
+            return "Hello " + target;
+        };
+    }
+}
