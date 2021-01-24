e74e3b6abee564138dd85d292258ca66bbf4a0eb
Dan Dobrin <dan.dobrin@gmail.com>
2021-01-24 18:38:07 -0500

    Updated function to return posted message and env var

--- src/main/java/com/example/hello/SpringNativeFunctionKnativeApplication.java
+++ src/main/java/com/example/hello/SpringNativeFunctionKnativeApplication.java
@@ -20,7 +20,7 @@ public class SpringNativeFunctionKnativeApplication {
     @Bean
     public Function<String, String> hello() {
         return (in) -> {
-            return "Hello " + target;
+            return "Hello: " + in + ", Source: " + target;
         };
     }
 }
